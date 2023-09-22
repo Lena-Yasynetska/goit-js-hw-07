637a16bf749280851e79bdeb14f6c883c0410fba
Lena-Yasynetska <123744975+Lena-Yasynetska@users.noreply.github.com>
2023-09-22 15:45:34 +0200

    7-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-
-// Change code below this line
 const galleryList = document.querySelector('.gallery');
 const galleryMarkup = galleryItems.map(item =>
-`<li class="gallery__item">
-<a class="gallery__link" href="${item.original}">
-<img
-class="gallery__image"
-src="${item.preview}"
-data-source="${item.original}"
-alt="${item.description}"
-/>
-</a>
-</li>`)
-.join('');
-galleryList.innerHTML = galleryMarkup;
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}" />
+    </a>
+    </li>`)
+    .join('');
 
- galleryList.addEventListener("click", e => {
+galleryList.innerHTML = galleryMarkup;
+galleryList.addEventListener("click", e => {
     e.preventDefault();
     if (e.target.tagName === "IMG") {
         const largeImageSrc = e.target.getAttribute("data-source");
-        const lightbox = basicLightbox.create(`
-        <img src="${largeImageSrc}" alt="${e.target.alt}" />`);
+        const lightbox = basicLightbox.create(`<img src="${largeImageSrc}" alt="${e.target.alt}" />`);
         lightbox.show();
     }
- });
-
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,31 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
 
 const ul = document.querySelector(".gallery");
 const markup = createMarkup(galleryItems);
 
 ul.insertAdjacentHTML('beforeend', markup);
-function createMarkup(arr){
-
-return arr.map(({preview, original, description}) => {
-      return `<li data preview = '${preview}' class='gallery__item js gallery__item'>
+function createMarkup(arr) {
+    return arr.map(({preview, original, description}) => {
+       return `<li data preview = '${preview}' class='gallery__item js gallery__item'>
        <a class = 'gallery__link' href = '${original}'>
-       <img 
-       class = "gallery__image" 
-       src = '${preview}'
-       alt = '${description}' />
+       <img class = "gallery__image" 
+          src = '${preview}'
+          alt = '${description}' />
        </a>
        </li>`
-})
+    })
 .join(" ");
 }
-// listEl.insertAdjacentHTML ('beforeend', markup);
-
-const lightboxModal = new SimpleLightbox(".gallery a",
- {
-    //caption: true,
+const lightboxModal = new SimpleLightbox(".gallery a", {
     captionsData: 'alt',
     captionPosition: 'bottom',
     captionDelay: 250,
